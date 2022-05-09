dfaa03006322267cb6ea8b7a299bae974a1eb411
asurek1 <asurek@ucsd.edu>
2022-05-08 23:02:13 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -39,7 +39,8 @@ public class MarkdownParse {
                 continue;
             }
             //if it is 100% a link, add it to the list
-            //new change
+            //new change for lab
+            //adding a new line
             else{
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
